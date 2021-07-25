bf12641f4af366d6d34bcc0ac1625afb6a8f14f5
Caitlin Watson-Beaton <42560844+watsbeat@users.noreply.github.com>
2021-07-25 19:08:13 +1000

    06-useRef-hook

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,9 +4,11 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef()
+
   function handleSubmit(event) {
     event.preventDefault()
-    const value = event.target.elements.usernameInput.value
+    const value = usernameInputRef.current.value
     value ? onSubmitUsername(value) : alert('No username provided.')
   }
 
@@ -14,7 +16,7 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" />
+        <input id="usernameInput" ref={usernameInputRef} type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
