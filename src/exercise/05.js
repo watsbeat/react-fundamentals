9982554b11e5aeba75d0e83bf593b83a830344e3
Caitlin Watson-Beaton <42560844+watsbeat@users.noreply.github.com>
2021-07-25 18:10:55 +1000

    05-accept-size-prop-to-encapsulate-styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,38 +4,39 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 💰 Use the className for the size and style (backgroundColor) for the color
-// 💰 each of the elements should also have the "box" className applied
+function Box({className = '', size, style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
 const smallBox = (
-  <div
-    className=" box box--small"
-    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
-  >
-    small lightblue box
-  </div>
+  <Box
+    size="small"
+    style={{backgroundColor: 'lightblue'}}
+    children="small lightblue box"
+  />
 )
 const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
-  >
-    medium pink box
-  </div>
+  <Box
+    size="medium"
+    style={{backgroundColor: 'pink'}}
+    children="medium pink box"
+  />
 )
 const largeBox = (
-  <div
-    className="box box--large"
-    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
-  >
-    large orange box
-  </div>
+  <Box
+    size="large"
+    style={{backgroundColor: 'orange'}}
+    children="large orange box"
+  />
 )
+const sizelessBox = <Box children="sizeless colourless box" />
 
 function App() {
   return (
@@ -43,6 +44,7 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      {sizelessBox}
     </div>
   )
 }
